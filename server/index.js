504105acee28349f186a957ecbea9930edc1f474
Jithin P Joji <jithinpjoji@gmail.com>
2026-02-11 11:33:06 +0530

    production build ready

--- server/index.js
+++ server/index.js
@@ -86,7 +86,7 @@ app.use('/api/admin', userRoutes);
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
